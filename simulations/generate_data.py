a0133ae5d8c7f2481a42bed747f819f26393b7ae
Liz Tan <elizabethsztan@gmail.com>
2025-05-08 21:42:28 +0800

    edit generate data

--- simulations/generate_data.py
+++ simulations/generate_data.py
@@ -15,8 +15,8 @@ def generate_data(sim = 'r1', save = True):
 
     Returns:
         X, y (tuple) containing:
-            - X (torch tensor)
-            - y (torch tensor)
+            - X (torch.tensor): dataset of shape [num_datapoints, num_particles, params]
+            - y (torch tensor): acceleration data of shape [num_datapoints, num_particles, num_dimension]
     """
 
     n_set = [4, 8] #could do 8 particles too but just working with 4 for now
@@ -32,11 +32,11 @@ def generate_data(sim = 'r1', save = True):
     ]
 
     sim_config = next(s for s in sim_sets if s['sim'] == sim)
-    n = sim_config['n'][0]
-    dim = sim_config['dim'][0]
-    nt = sim_config['nt'][0]
-    dt = sim_config['dt'][0]
-    ns = 10_000
+    n = sim_config['n'][0] #number of particles 'nodes'
+    dim = sim_config['dim'][0] #dimension
+    nt = sim_config['nt'][0] #number of timesteps
+    dt = sim_config['dt'][0] #discretised time
+    ns = 10_000 #number of simulations to run
 
     title = '{}_n={}_dim={}_nt={}_dt={}'.format(sim, n, dim, nt, dt)
     print('Running on', title)
@@ -46,7 +46,11 @@ def generate_data(sim = 'r1', save = True):
 
     accel_data = s.get_acceleration()
 
+    #take every 5th datapoint from each simulation
+    #nearby datapoints are correlated 
     X = torch.from_numpy(np.concatenate([s.data[:, i] for i in range(0, s.data.shape[1], 5)]))
+
+    #acceleration data (target)
     y = torch.from_numpy(np.concatenate([accel_data[:, i] for i in range(0, s.data.shape[1], 5)]))
 
     if save: 
@@ -60,9 +64,9 @@ def generate_data(sim = 'r1', save = True):
 
     return X, y
 
-# def load_data(path):
-#     data = torch.load(f"{path}.pt")
-#     return data['X'], data['y']
+def load_data(path):
+    data = torch.load(f"{path}.pt")
+    return data['X'], data['y']
 
 def main():
     parser = argparse.ArgumentParser()
