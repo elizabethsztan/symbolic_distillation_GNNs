5673c56e8b4f5f875b37b1fa324f44940eac3ec3
Liz Tan <elizabethsztan@gmail.com>
2025-05-14 14:50:32 +0100

    updated model and training func for wandb tracking and manual logging. merged models

--- model.py
+++ model.py
@@ -3,6 +3,14 @@ import torch.nn as nn
 from torch_geometric.nn import MessagePassing
 from torch.utils.data import Dataset, DataLoader
 from tqdm import tqdm
+from accelerate import Accelerator
+from torch.optim.lr_scheduler import OneCycleLR
+import wandb
+import os
+from datetime import datetime
+import json
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
 
 def get_edge_index(num_nodes): #edge index for fully connected graph
     idx = torch.arange(num_nodes)
@@ -13,6 +21,27 @@ def get_edge_index(num_nodes): #edge index for fully connected graph
 def load_data(path): #load dataset 
     data = torch.load(f"{path}.pt")
     return data['X'], data['y']
+
+class NBodyDataset(Dataset):
+    """
+    Create pytorch dataset class for our simulation dataset.
+    """
+    def __init__(self, data, targets):
+        """
+        Args:
+            data (torch.Tensor): shape is [no_datapoints, no_nodes, no_node_features]
+            targets (torch.Tensor): shape is [no_datapoints, no_nodes, 2d_acceleration]
+        """
+        self.data = data
+        self.targets = targets
+
+    def __len__(self):
+        return len(self.data) #how many samples in dataset to use later for batching
+
+    def __getitem__(self, idx):
+        nodes = self.data[idx]  # shape: [no_nodes, no_node_features] 
+        acc = self.targets[idx]
+        return nodes, acc #inputs and target variables
 
 class NBodyGNN(MessagePassing):
     def __init__(self, node_dim = 6, acc_dim = 2, hidden_dim = 300):
@@ -45,14 +74,24 @@ class NBodyGNN(MessagePassing):
             nn.Linear(hidden_dim, acc_dim) #output = predicted acc
         )
 
-        self.node_dim0 = node_dim
-        self.acc_dim0 = acc_dim
-        self.hidden_dim0 = hidden_dim
+        self.message_features = None
+        self.combined_messages = None
+
+        self.node_dim_ = node_dim
+        self.acc_dim_ = acc_dim
+        self.hidden_dim_ = hidden_dim
 
     def message(self, x_i, x_j):
         x = torch.cat((x_i, x_j), dim = -1) #concat along final dimension = features. shape is [batch_size, no_edges, no_features]
-        x = self.edge_model(x) #put thru MLP. MLP only transforms the feature (last) dimension
-        return x
+        message_features = self.edge_model(x) #put thru MLP. MLP only transforms the feature (last) dimension
+        
+        if self.message_features == None:
+            self.message_features = message_features
+        
+        else:
+            self.message_features = torch.cat([self.message_features, message_features], dim=0)
+        
+        return message_features
 
     def forward(self, x, edge_index): 
         """Forward pass of this network
@@ -61,96 +100,87 @@ class NBodyGNN(MessagePassing):
             x (torch.Tensor): shape is [batch_size, no_nodes, no_node_features]
             edge_index (torch.Tensor): shape is [2, no_edges]
         """
-
+        self.message_features = None
         edge_message =  self.propagate(edge_index, x = (x,x)) #use same feature matrix for both source and target nodes (undirected network)
         #x is shape [batch_size, no_nodes, no_features]
         acc_pred = self.node_model(torch.cat([x, edge_message], dim = -1)) #predict accelerations
 
         return acc_pred
     
-class NBodyGNN_L1(NBodyGNN):
-    def __init__(self, node_dim=6, acc_dim=2, hidden_dim=300):
-        super().__init__(node_dim=node_dim, acc_dim=acc_dim, hidden_dim=hidden_dim)
-        self.message_features = None
-        self.combined_messages = None
-    
-    def message(self, x_i, x_j):
-        message_features = super().message(x_i, x_j)
-
-        if self.message_features == None:
-            self.message_features = message_features
-
-        else:
-            self.message_features = torch.cat([self.message_features, message_features], dim=0)
-
-        return message_features
-    
-    def forward(self, x, edge_index):
-        self.message_features = None
-
-        acc_pred = super().forward(x, edge_index)
-
-        return acc_pred
-    
     def get_messages(self):
         return self.message_features
-    
-class NBodyDataset(Dataset):
-    """
-    Create pytorch dataset class for our simulation dataset.
-    """
-    def __init__(self, data, targets):
-        """
-        Args:
-            data (torch.Tensor): shape is [no_datapoints, no_nodes, no_node_features]
-            targets (torch.Tensor): shape is [no_datapoints, no_nodes, 2d_acceleration]
-        """
-        self.data = data
-        self.targets = targets
 
-    def __len__(self):
-        return len(self.data) #how many samples in dataset to use later for batching
-
-    def __getitem__(self, idx):
-        nodes = self.data[idx]  # shape: [no_nodes, no_node_features] 
-        acc = self.targets[idx]
-        return nodes, acc #inputs and target variables
-
-def train (train_data, val_data, num_epoch, hidden_dim=300, patience = 10):
+def train(model, train_data, val_data, num_epoch, dataset_name = 'r2', model_type = 'standard', save = True, wandb_log = True):
     """ Train the GNN
 
     Args:
+        model (NBodyGNN): the model class
         train_data (tuple): contains (input_data, accelerations)
         val_data (tuple): contains (input_data, accelerations)
         num_epoch (int): number of epochs to train on
-        patience (int): number of epochs to wait before implementing early stopping
 
     Returns:
         model (NBodyGNN object): final trained model
     """
-
     #training data
     input_data, acc = train_data
+
+    if model.node_dim_ != input_data.shape[-1]:
+        assert 'Mismatch in model and data node/particle dimensions.'
+        
     dataset = NBodyDataset(input_data, acc)   
-    dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
+    batch_size = int(64*(4/input_data.shape[1])**2) #batch depends on num_nodes
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
 
     #validation data
     input_val, acc_val = val_data
     val_dataset = NBodyDataset(input_val, acc_val)
-    val_dataloader = DataLoader(val_dataset, batch_size=32, shuffle=True)
+    val_dataloader = DataLoader(val_dataset, batch_size=1024, shuffle=False)
 
-    node_dim = input_data.shape[-1]
+    # node_dim = input_data.shape[-1]
+    num_nodes = input_data.shape[1]
     acc_dim = acc.shape[-1]
 
-    model = NBodyGNN(node_dim=node_dim, acc_dim=acc_dim, hidden_dim=hidden_dim)
-    optimiser = torch.optim.Adam(model.parameters(), weight_decay=1e-8) #L2 regulariser on params
+    if wandb_log:
+        mode = 'online'
+    else:
+        mode = 'disabled'
+
+    wandb.init(project = f'{dataset_name}',
+            name = f'{model_type}',
+            config={'num_epochs': num_epoch, 'num_nodes': num_nodes, 'dim': acc_dim}, 
+            mode = mode
+            )
+
+    #optimiser
+    init_lr = 1e-3
+    optimiser = torch.optim.Adam(model.parameters(), init_lr, weight_decay=1e-8) #L2 regulariser on params
+
+    #load onto GPU
+    accelerator = Accelerator()
+    model, dataloader, val_dataloader, optimiser = accelerator.prepare(model, dataloader, val_dataloader, optimiser)
+
+    print(f'Running on {accelerator.device}.')
+
+    #learning rate scheduler
+    scheduler = OneCycleLR(optimiser, max_lr = init_lr, steps_per_epoch = len(dataloader), epochs = num_epoch, final_div_factor = 1e5)
     criterion = nn.L1Loss() #MAE loss
 
-    edge_index = get_edge_index(input_data.shape[1]) #this never changes so we only calc once
+    #regularisation 
+    if model_type == 'standard':
+        def regularisation(model):
+            return 0.0
+        
+    elif model_type == 'L1':
+        def regularisation (model):
+            message_features = model.get_messages()
+            return torch.mean(torch.abs(message_features))
 
-    best_val_loss = float('inf')
-    best_model_state = None
-    counter = 0
+
+    edge_index = get_edge_index(input_data.shape[1]).to(accelerator.device) #this never changes so we only calc once
+
+    losses = []
+    val_losses = []
 
     for epoch in range (num_epoch):
         total_loss = 0 #loss tracking
@@ -166,15 +196,16 @@ def train (train_data, val_data, num_epoch, hidden_dim=300, patience = 10):
 
             acc_pred = model(nodes, edge_index) #automatically calls model.forward()
             
-            loss = criterion(acc_pred, acc)
+            loss = criterion(acc_pred, acc) + regularisation(model)
 
-            loss.backward()
+            accelerator.backward(loss)
             optimiser.step()
+            scheduler.step()
 
             total_loss += loss.item()
             #pbar.set_postfix({"batch_loss": f"{loss.item():.4f}"})
 
-        #validation 
+        #validation once per epoch
         model.eval()
         val_loss = 0
         with torch.no_grad(): #stop computing gradients
@@ -186,172 +217,35 @@ def train (train_data, val_data, num_epoch, hidden_dim=300, patience = 10):
         avg_val_loss = val_loss/len(val_dataloader)
         print(f'training loss: {avg_loss:.4f}, val loss: {avg_val_loss:.4f}')
 
-        #check if this is the best loss
-        if avg_val_loss < best_val_loss:
-            best_val_loss = avg_val_loss
-            best_model_state = model.state_dict().copy()
-            counter = 0
-            print(f"new best validation loss: {best_val_loss:.4f}")
-        
-        else: 
-            counter += 1
-            print(f"EarlyStopping counter: {counter} out of {patience}")
+        #log to wandb
+        wandb.log({
+            'epoch': epoch, 
+            'train_loss': avg_loss,
+            'val_loss': avg_val_loss,
+        })
 
-        if counter >= patience:
-            print(f"Early stopping triggered after {epoch+1} epochs")
-            break
+        #save losses
+        losses.append(avg_loss)
+        val_losses.append(avg_val_loss)
 
-    if best_model_state is not None:
-        model.load_state_dict(best_model_state)
-        print(f"Loaded best model with validation loss: {best_val_loss:.4f}")
+    wandb.finish()
 
-
-    return model
-
-def train_L1 (train_data, val_data, num_epoch, hidden_dim=300, patience = 10):
-    """ Train the GNN with L1 regularisation on the messages
-
-    Args:
-        train_data (tuple): contains (input_data, accelerations)
-        val_data (tuple): contains (input_data, accelerations)
-        num_epoch (int): number of epochs to train on
-        patience (int): number of epochs to wait before implementing early stopping
-
-    Returns:
-        model (NBodyGNN object): final trained model
-    """
-
-    #training data
-    input_data, acc = train_data
-    dataset = NBodyDataset(input_data, acc)   
-    dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
-
-    #validation data
-    input_val, acc_val = val_data
-    val_dataset = NBodyDataset(input_val, acc_val)
-    val_dataloader = DataLoader(val_dataset, batch_size=32, shuffle=True)
-
-    node_dim = input_data.shape[-1]
-    acc_dim = acc.shape[-1]
-
-    model = NBodyGNN_L1(node_dim=node_dim, acc_dim=acc_dim, hidden_dim=hidden_dim)
-    optimiser = torch.optim.Adam(model.parameters(), weight_decay=1e-8) #L2 regulariser on params
-    criterion = nn.L1Loss() #MAE loss
-
-    edge_index = get_edge_index(input_data.shape[1]) #this never changes so we only calc once
-    #num_edges = int(len(edge_index[0])/2)
-
-    best_val_loss = float('inf')
-    best_model_state = None
-    counter = 0
-
-    for epoch in range (num_epoch):
-        total_loss = 0 #loss tracking
-        
-        #set model in training mode
-        model.train()
-
-        pbar = tqdm(dataloader, desc=f"Epoch: {epoch+1}/{num_epoch}")
-        for nodes, acc in pbar:
-
-            #training
-            optimiser.zero_grad()
-
-            acc_pred = model(nodes, edge_index) #automatically calls model.forward()
-            message_features = model.get_messages() #get message features. of shape [batch_size, num_nodes, 100]
-            
-            loss = criterion(acc_pred, acc) + 1e-2 * torch.mean(torch.abs(message_features)) #add L1 regulariser on messages
-
-            loss.backward()
-            optimiser.step()
-
-            total_loss += loss.item()
-            #pbar.set_postfix({"batch_loss": f"{loss.item():.4f}"})
-
-        #validation 
-        model.eval()
-        val_loss = 0
-        with torch.no_grad(): #stop computing gradients
-            for nodes, acc in val_dataloader:
-                acc_pred = model(nodes, edge_index) #run forward pass thru model
-                val_loss += criterion(acc_pred, acc).item()
-
-        avg_loss = total_loss/len(dataloader)
-        avg_val_loss = val_loss/len(val_dataloader)
-        print(f'training loss: {avg_loss:.4f}, val loss: {avg_val_loss:.4f}')
-        #here it prints the loss INCLUDING the L1 loss term
-        #you may want to have it just printing the accuracy? doesnt actually matter
-
-        #check if this is the best loss
-        if avg_val_loss < best_val_loss:
-            best_val_loss = avg_val_loss
-            best_model_state = model.state_dict().copy()
-            counter = 0
-            print(f"new best validation loss: {best_val_loss:.4f}")
-        
-        else: 
-            counter += 1
-            print(f"EarlyStopping counter: {counter} out of {patience}")
-
-        if counter >= patience:
-            print(f"Early stopping triggered after {epoch+1} epochs")
-            break
-
-    if best_model_state is not None:
-        model.load_state_dict(best_model_state)
-        print(f"Loaded best model with validation loss: {best_val_loss:.4f}")
-
+    if save:
+        save_path = f'{script_dir}/model_weights/{dataset_name}/{model_type}'
+        os.makedirs(save_path, exist_ok=True)
+        #save model weights
+        torch.save(model.state_dict(), f'{save_path}/epoch_{num_epoch}_model.pth')
+        #log training run in json file
+        metrics = {'datetime':datetime.now().strftime("%Y-%m-%d %H:%M:%S"), 
+                   'dataset_name': dataset_name,
+                   'model_type': model_type,
+                   'epochs': num_epoch,
+                   'num_nodes': num_nodes,
+                   'dimensions': acc_dim, 
+                   'train_loss': losses,
+                   'val_loss': val_losses
+                   }
+        with open(f'{save_path}/epoch_{num_epoch}_metrics.json', 'w') as json_file:
+            json.dump(metrics, json_file, indent = 4)
 
     return model
-
-def save_model(model, directory = "testing_models/model.pt"):
-    checkpoint = {
-        'model_state_dict': model.state_dict(),     # Model weights
-        'edge_model': model.edge_model.state_dict(),  # Edge MLP state
-        'node_model': model.node_model.state_dict(),  # Node MLP state
-        'node_dim': model.node_dim0,        # Your model's dimensions from __init__
-        'acc_dim': model.acc_dim0,         # Acceleration dimensions (2 for 2D)
-        'hidden_dim': model.hidden_dim0    # Hidden layer dimensions
-        } 
-    torch.save(checkpoint, f"{directory}")
-    print('Model saved successfully')
-
-def load_model(directory):
-    checkpoint = torch.load(f"{directory}")
-    model = NBodyGNN(
-        node_dim=checkpoint['node_dim'],
-        acc_dim=checkpoint['acc_dim'],
-        hidden_dim=checkpoint['hidden_dim'])
-    model.load_state_dict(checkpoint['model_state_dict'])
-
-    print('Model loaded successfully')
-
-    return model
-
-def test(test_data, model):
-    """
-    Get loss for a test dataset
-    """
-
-    input_data, acc = test_data
-    dataset = NBodyDataset(input_data, acc)
-    dataloader = DataLoader(dataset, batch_size=32, shuffle=False)
-
-    edge_index = get_edge_index(input_data.shape[1])
-
-    criterion = nn.L1Loss() #can add optionality to change this later on...
-    
-    model.eval()
-    loss = 0
-    with torch.no_grad():
-        for nodes, acc in dataloader:
-            acc_pred = model(nodes, edge_index)
-            loss += criterion(acc_pred, acc).item()
-    
-    avg_loss = loss/len(dataloader)
-    print('Avg loss: ', avg_loss)
-
-    return avg_loss
-
-
-
