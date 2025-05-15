f82b129b7f15abd27f4cd22254e8767ad7cac315
Liz Tan <elizabethsztan@gmail.com>
2025-05-15 17:19:00 +0100

    created load and preprocess func to split data into train test val only once

---
+++ utils.py
@@ -0,0 +1,30 @@
+import os
+from sklearn.model_selection import train_test_split
+import torch
+
+def load_and_process(data_dir, seed):
+    filename = os.path.basename(data_dir).split('.')[0]
+    data_save_path = f'train_val_test_data/{filename}'
+    
+    if not os.path.exists(data_save_path):
+        os.makedirs(data_save_path, exist_ok=True)
+        
+        data = torch.load(data_dir)
+        X, y = data['X'], data['y']
+        
+        X_train, X_val, y_train, y_val = train_test_split(X, y, shuffle=True, test_size=0.3, random_state=seed)
+        X_val, X_test, y_val, y_test = train_test_split(X_val, y_val, shuffle=True, test_size=0.5, random_state=seed)
+        
+        train_data = (X_train, y_train)
+        val_data = (X_val, y_val)
+        test_data = (X_test, y_test)
+        
+        torch.save(train_data, os.path.join(data_save_path, 'train_data.pt'))
+        torch.save(val_data, os.path.join(data_save_path, 'val_data.pt'))
+        torch.save(test_data, os.path.join(data_save_path, 'test_data.pt'))
+    else:
+        train_data = torch.load(os.path.join(data_save_path, 'train_data.pt'))
+        val_data = torch.load(os.path.join(data_save_path, 'val_data.pt'))
+        test_data = torch.load(os.path.join(data_save_path, 'test_data.pt'))
+    
+    return train_data, val_data, test_data
