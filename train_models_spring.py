3b7824bbbf81cacde68dde6663bc0d1ea719a1c6
Liz Tan <elizabethsztan@gmail.com>
2025-05-18 17:56:48 +0100

    trained on spring

---
+++ train_models_spring.py
@@ -0,0 +1,30 @@
+from model import *
+from utils import *
+import argparse
+
+#split the data into train and test sets
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--save', action='store_true')
+    parser.add_argument('--wandb_log', action='store_true')
+    
+    args = parser.parse_args()
+    
+    seed = 290402 
+    data_path = 'datasets/spring_n=4_dim=2_nt=1000_dt=0.01.pt'
+
+    train_data, val_data, _ = load_and_process(data_path, seed)
+
+    # print("\n=== Testing 'standard' Model ===")
+    # model = NBodyGNN()
+    # model = train(model, train_data=train_data, val_data=val_data, dataset_name = 'spring', num_epoch=200, 
+    #              model_type='standard', save=args.save, wandb_log=args.wandb_log)
+
+    print("\n=== Testing L1 Model ===")
+    model = NBodyGNN()
+    model = train(model, train_data=train_data, val_data=val_data, dataset_name= 'spring', num_epoch=100, 
+                 model_type='L1', save=args.save, wandb_log=args.wandb_log)
+
+if __name__ == "__main__":
+    main()
