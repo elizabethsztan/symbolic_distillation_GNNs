580fda40be54718d1fbc1fe8d4f9e72b3a6cd7dd
Liz Tan <elizabethsztan@gmail.com>
2025-05-19 16:58:08 +0100

    added comments

--- utils.py
+++ utils.py
@@ -6,7 +6,7 @@ def load_and_process(data_dir, seed):
     filename = os.path.basename(data_dir).split('_')[0]
     data_save_path = f'train_val_test_data/{filename}'
     
-    if not os.path.exists(data_save_path):
+    if not os.path.exists(data_save_path): #checks if the data has already been split
         os.makedirs(data_save_path, exist_ok=True)
         
         data = torch.load(data_dir)
@@ -22,14 +22,14 @@ def load_and_process(data_dir, seed):
         torch.save(train_data, os.path.join(data_save_path, 'train_data.pt'))
         torch.save(val_data, os.path.join(data_save_path, 'val_data.pt'))
         torch.save(test_data, os.path.join(data_save_path, 'test_data.pt'))
-    else:
+    else: #load the data if the data has already been split
         train_data = torch.load(os.path.join(data_save_path, 'train_data.pt'))
         val_data = torch.load(os.path.join(data_save_path, 'val_data.pt'))
         test_data = torch.load(os.path.join(data_save_path, 'test_data.pt'))
     
     return train_data, val_data, test_data
 
-def load_data (dataset_name):
+def load_data (dataset_name): #load the data
     path = f'train_val_test_data/{dataset_name}'
 
     train_data = torch.load(os.path.join(path, 'train_data.pt'))
