dde57510a5bb4cc5b0b926b7b17aacb8cb36763a
Liz Tan <elizabethsztan@gmail.com>
2025-05-22 18:41:57 +0100

    sorted my sr file

--- symbolic_reg.py
+++ symbolic_reg.py
@@ -1,27 +1,37 @@
+import pysr
 from plot_linear_rep import get_message_features
 from model import load_model
 from utils import load_data
 import numpy as np
-import os
+# import os
+# import sympy
 from pysr import PySRRegressor
+print("PySR imported successfully without warnings!")
 
 def get_pysr_variables(model, input_data):
-    sr_vars = ['dy', 'dx', 'r', 'm1', 'm2'] #you can change this if you want to include others in the regression
+    sr_vars = ['dx', 'dy', 'r', 'm1', 'm2'] #you can change this if you want to include others in the regression
 
     print(f'Variables considered in the SR are {sr_vars}.')
 
-    #need to add option for KL too. choosing the best message now becomes different 
+    #getting message for KL is different 
+    print('Getting messages.')
     if model.model_type_ != 'KL':
-        print('Getting messages.')
         df, msg_array = get_message_features(model, input_data)
         msg_importance = msg_array.std(axis=0) #computes stds for each msg_dim over all datapoints
         most_important = np.argsort(msg_importance)[-1:]
+    else: 
+        df, msg_array = get_message_features(model, input_data)
+        logvar_array = msg_array[1]
+        msg_array = msg_array[0]
+        KL_div =  (np.exp(logvar_array) + msg_array**2 - logvar_array)/2
+        KL_mean = KL_div.mean(axis=0)
+        most_important = np.argsort(KL_mean)[-1:]
     target_message = df[f'e{most_important[0]}'].to_numpy().reshape(-1, 1)
     variables = df[sr_vars].to_numpy()
 
     return target_message, variables
 
-def perform_sr(target_message, variables, num_points = 5000):
+def perform_sr(target_message, variables, num_points = 10_000):
     np.random.seed(290402)
     #get a smaller random subset of points because we have too many edge messages
     idx = np.random.choice(len(target_message), size=num_points, replace=False)
@@ -32,20 +42,22 @@ def perform_sr(target_message, variables, num_points = 5000):
     #set up the regressor
     regressor = PySRRegressor(
         maxsize=20,
-        niterations=40,  # < Increase me for better results
+        niterations=2000,  # < Increase me for better results
         binary_operators=["+", "*"],
         unary_operators=[
             "inv(x) = 1/x"
         ],
         extra_sympy_mappings={"inv": lambda x: 1 / x},
         # ^ Define operator for SymPy as well
-        elementwise_loss="loss(prediction, target) = (prediction - target)^2",
+        elementwise_loss="loss(prediction, target) = abs(prediction - target)",
         # ^ Custom loss function (julia syntax)
     )
 
     regressor.fit(variables_subset, target_subset)
 
-    print(f'The best equation found was {regressor.get_best()['equation']}')
+    best_eq = regressor.get_best()['equation']
+    print(f'The best equation found was {best_eq}')
+
 
     return regressor
 
@@ -54,3 +66,7 @@ def main():
     _, _, test_data = load_data('spring')
     target_message, variables = get_pysr_variables(model, test_data)
     regressor = perform_sr(target_message, variables)
+    # print(regressor.get_best()['equation'])
+
+if __name__ == "__main__":
+    main()
