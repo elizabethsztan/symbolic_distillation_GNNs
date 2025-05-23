3f75620ed49614cbd218c332e5ed5c8893987490
Liz Tan <elizabethsztan@gmail.com>
2025-05-23 11:23:36 +0100

    changed parsimony hyperparm

--- symbolic_reg.py
+++ symbolic_reg.py
@@ -3,10 +3,8 @@ from plot_linear_rep import get_message_features
 from model import load_model
 from utils import load_data
 import numpy as np
-# import os
-# import sympy
 from pysr import PySRRegressor
-print("PySR imported successfully without warnings!")
+print("PySR imported successfully!")
 
 def get_pysr_variables(model, input_data):
     sr_vars = ['dx', 'dy', 'r', 'm1', 'm2'] #you can change this if you want to include others in the regression
@@ -31,7 +29,7 @@ def get_pysr_variables(model, input_data):
 
     return target_message, variables
 
-def perform_sr(target_message, variables, num_points = 10_000):
+def perform_sr(target_message, variables, num_points = 5_000, niterations = 1000):
     np.random.seed(290402)
     #get a smaller random subset of points because we have too many edge messages
     idx = np.random.choice(len(target_message), size=num_points, replace=False)
@@ -42,31 +40,30 @@ def perform_sr(target_message, variables, num_points = 10_000):
     #set up the regressor
     regressor = PySRRegressor(
         maxsize=20,
-        niterations=2000,  # < Increase me for better results
+        niterations=niterations,  # < Increase me for better results
         binary_operators=["+", "*"],
         unary_operators=[
             "inv(x) = 1/x"
         ],
         extra_sympy_mappings={"inv": lambda x: 1 / x},
-        # ^ Define operator for SymPy as well
-        elementwise_loss="loss(prediction, target) = abs(prediction - target)",
-        # ^ Custom loss function (julia syntax)
+        elementwise_loss="loss(prediction, target) = abs(prediction - target)", #MAE loss
+        parsimony=0.05, #penalise complexity
+        batching=True
     )
 
     regressor.fit(variables_subset, target_subset)
 
-    best_eq = regressor.get_best()['equation']
-    print(f'The best equation found was {best_eq}')
-
+    # best_eq = regressor.get_best()['equation']
+    # print(f'The best equation found was {best_eq}')
 
     return regressor
 
 def main():
-    model = load_model(dataset_name='spring', model_type='bottleneck', num_epoch='100')
+    model = load_model(dataset_name='spring', model_type='L1', num_epoch='100')
     _, _, test_data = load_data('spring')
     target_message, variables = get_pysr_variables(model, test_data)
-    regressor = perform_sr(target_message, variables)
-    # print(regressor.get_best()['equation'])
+    regressor = perform_sr(target_message, variables, niterations=1000)
+    print(regressor.get_best()['equation'])
 
 if __name__ == "__main__":
     main()
