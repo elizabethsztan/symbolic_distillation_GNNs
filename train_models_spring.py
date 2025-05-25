9ad9c6d0e620a0e78aba998e9cd01cfb5e025494
Liz Tan <elizabethsztan@gmail.com>
2025-05-25 17:29:07 +0100

    train model with new pruning schedule

--- train_models_spring.py
+++ train_models_spring.py
@@ -17,35 +17,35 @@ def main():
 
     train_data, val_data, _ = load_and_process(data_path, seed)
 
-    print("\n=== Testing standard Model ===")
-    model_type = 'standard'
-    model = create_model(model_type = model_type)
-    model = train(model, train_data=train_data, val_data=val_data, dataset_name = 'spring', num_epoch=args.epoch,
-                  save=args.save, wandb_log=args.wandb_log)
+    # print("\n=== Testing standard Model ===")
+    # model_type = 'standard'
+    # model = create_model(model_type = model_type)
+    # model = train(model, train_data=train_data, val_data=val_data, dataset_name = 'spring', num_epoch=args.epoch,
+    #               save=args.save, wandb_log=args.wandb_log)
 
     # print("\n=== Testing L1 Model ===")
     # model_type = 'L1'
     # model = create_model(model_type = model_type)
     # model = train(model, train_data=train_data, val_data=val_data, dataset_name = 'spring', num_epoch=args.epoch,
     #               save=args.save, wandb_log=args.wandb_log)
-    
+
     # print("\n=== Testing bottleneck Model ===")
     # model_type = 'bottleneck'
     # model = create_model(model_type = model_type)
     # model = train(model, train_data=train_data, val_data=val_data, dataset_name = 'spring', num_epoch=args.epoch,
     #               save=args.save, wandb_log=args.wandb_log)
-    
+
     # print("\n=== Testing KL Model ===")
     # model_type = 'KL'
     # model = create_model(model_type = model_type)
     # model = train(model, train_data=train_data, val_data=val_data, dataset_name = 'spring', num_epoch=args.epoch,
     #               save=args.save, wandb_log=args.wandb_log)
-    
-    # print("\n=== Testing pruning Model ===")
-    # model_type = 'pruning'
-    # model = create_model(model_type = model_type)
-    # model = train(model, train_data=train_data, val_data=val_data, dataset_name = 'spring', num_epoch=args.epoch,
-    #               save=args.save, wandb_log=args.wandb_log)
+
+    print("\n=== Testing pruning Model ===")
+    model_type = 'pruning'
+    model = create_model(model_type = model_type)
+    model = train(model, train_data=train_data, val_data=val_data, dataset_name = 'spring', num_epoch=args.epoch,
+                  save=args.save, wandb_log=args.wandb_log)
     
 
 if __name__ == "__main__":
