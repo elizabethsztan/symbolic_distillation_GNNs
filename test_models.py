b19ab328e3b392ee146f6276edcddf0dce593198
Liz Tan <elizabethsztan@gmail.com>
2025-06-23 14:12:55 +0100

    comments

--- test_models.py
+++ test_models.py
@@ -4,18 +4,26 @@ import json
 import os
 
 seed = 290402 
+"""
+This script runs the test set through all the trained model variations to get prediction losses (used in the report).
+We assume that all the models (standard, L1, bottleneck, KL, pruning) have been trained on all the simulations 
+(charge, r1, r2, spring) for 100 epochs.
+"""
 
 data_paths = ['datasets/charge_n=4_dim=2_nt=1000_dt=0.001.pt', 'datasets/r1_n=4_dim=2_nt=1000_dt=0.005.pt', 'datasets/r2_n=4_dim=2_nt=1000_dt=0.01.pt', 'datasets/spring_n=4_dim=2_nt=1000_dt=0.01.pt']
 simulation_names = ['charge', 'r1', 'r2', 'spring']
 model_types = ['standard', 'bottleneck', 'L1', 'KL', 'pruning']
 
+#save the prediction losses here
 metrics = {}
 
 for i, data_path in enumerate(data_paths):
+    #iterate thru all datasets
     _, _, test_data = load_and_process(data_path, seed)
     sim_name = simulation_names[i]
     metrics[sim_name] = {}
     
+    #iterate thru all model types
     for model_type in model_types:
         print(f"Testing {sim_name} - {model_type}")
         model = load_model(simulation_names[i], model_type, 100)
@@ -26,6 +34,7 @@ for i, data_path in enumerate(data_paths):
 save_path = 'model_weights'
 os.makedirs(save_path, exist_ok=True)
 
+#save results
 with open(f'{save_path}/test_results.json', 'w') as f:
     json.dump(metrics, f, indent=2)
 
